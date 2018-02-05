aadbb75106be8325f26daae8caf27eab20d75d27
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-02-04 23:30:49 -0700

    Update linkedlist.h

--- HW2/problem6/linkedlist.h
+++ HW2/problem6/linkedlist.h
@@ -19,6 +19,17 @@ struct node obj;
 #define GET_LIST_CONTAINER(ptr, type, member) ({                \
         const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
         (type *)( (char *)__mptr - offsetof(type,member) );})
+
+void destroy(struct info* head);
+struct info* insert_at_beginning(struct info* head, uint32_t userdata);
+struct info* insert_at_end(struct info *head, uint32_t userdata);
+struct info* insert_at_position(struct info *head, uint32_t userdata, int index);
+struct info* delete_from_beginning(struct info *head);
+struct info* delete_from_end(struct info *head);
+struct info* delete_from_position(struct info *head, int index);
+int size(struct info* head);
+void display(struct info* head);
+uint32_t peak(struct info* head, int index);
         
         
 #endif
