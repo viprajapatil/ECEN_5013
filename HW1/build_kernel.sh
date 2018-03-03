dc9148467f6dfb3b065ec7e88b081c6f3551eaa1
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-03-02 22:15:10 -0700

    Add files via upload

---
+++ HW1/build_kernel.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#change directory
+cd /home/vipraja/kernel/linux-4.14.13
+
+make menuconfig
+make -j2 > kernel-build.log
+sudo make modules_install >> kernel-build.log
+sudo make install >> kernel-build.log
+
+
