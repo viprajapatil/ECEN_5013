458e403d0d37040cf4850b70c8eebe447833f676
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-03-06 17:17:49 -0700

    Update and rename ipc_msg_queue_server.c to ipc_msg_queue_one.c

---
+++ HW4/Problem 2/Message queue/ipc_msg_queue_one.c
@@ -0,0 +1,68 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <mqueue.h>
+
+
+typedef struct{
+	char string[50];
+	int string_length;
+	int USR_Led_Control;
+}message;
+
+#define SERVER_QUEUE_NAME   "/my_msg_queue_server"
+#define QUEUE_PERMISSIONS 0660
+#define MAX_MESSAGES 10
+#define MAX_MSG_SIZE 256
+#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
+
+int main()
+{
+	mqd_t server, client;   // queue descriptors   
+    struct mq_attr attr;
+	
+	message *ptr;
+    message mesg_struct;
+    ptr = &mesg_struct;
+	 char buff[sizeof(message)] = {0};
+	
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = MAX_MESSAGES;
+    attr.mq_msgsize = MAX_MSG_SIZE;
+    attr.mq_curmsgs = 0;
+	
+	server = mq_open (SERVER_QUEUE_NAME, O_RDWR | O_CREAT, 0666, &attr);
+	if (server < 0)
+		printf("ERROR opening message queue\n");
+	
+	//char* buffrec = (char*)(&mesg_struct);
+	//receive a message from a message queue
+	char *payload_cptr;
+    	message payloadRecv = {0};
+    	payload_cptr = (char*)&payloadRecv;
+	
+	if (mq_receive (server, payload_cptr, sizeof(mesg_struct), 0) < 0)
+		printf("ERROR mq_receive\n");
+	else printf ("Process 1: message received.\n");
+	
+	ptr = (message*)(buff);
+    printf("Process1 Received string: %s, String length = %d, USR led status: %d\n", ptr->string, ptr->string_length, ptr->USR_Led_Control);
+	
+	//send message to process 2
+	strcpy(mesg_struct.string, "From Process 1 to Process 2 msg\n");
+    mesg_struct.string_length = strlen(mesg_struct.string);
+    mesg_struct.USR_Led_Control = 0;
+	char* buffptr = (char*)(&mesg_struct);
+	if (mq_send (server, buffptr, sizeof(message), 0) < 0)
+		printf("ERROR mq_send\n");
+	else printf("Process 1: message sent\n");
+
+	mq_close(server);
+	
+	
+	return 0;
+}
