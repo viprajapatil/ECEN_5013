072a01a15794dc724ce73c4a9869ad61e20ba9a4
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-03-06 18:46:19 -0700

    Update ipc_msg_queue_one.c

--- HW4/Problem 2/Message queue/ipc_msg_queue_one.c
+++ HW4/Problem 2/Message queue/ipc_msg_queue_one.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +5,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
+#include <errno.h>
 
 
 typedef struct{
@@ -15,14 +15,14 @@ typedef struct{
 }message;
 
 #define SERVER_QUEUE_NAME   "/my_msg_queue_server"
-#define QUEUE_PERMISSIONS 0660
+#define QUEUE_PERMISSIONS 0666
 #define MAX_MESSAGES 10
 #define MAX_MSG_SIZE 256
 #define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
 
 int main()
 {
-	mqd_t server, client;   // queue descriptors   
+    mqd_t server;   // queue descriptors   
     struct mq_attr attr;
 	
 	message *ptr;
@@ -30,39 +30,38 @@ int main()
     ptr = &mesg_struct;
 	 char buff[sizeof(message)] = {0};
 	
-    attr.mq_flags = 0;
+    //attr.mq_flags = 0;
     attr.mq_maxmsg = MAX_MESSAGES;
-    attr.mq_msgsize = MAX_MSG_SIZE;
-    attr.mq_curmsgs = 0;
+    attr.mq_msgsize = sizeof(message);
+    //attr.mq_curmsgs = 0;
 	
-	server = mq_open (SERVER_QUEUE_NAME, O_RDWR | O_CREAT, 0666, &attr);
-	if (server < 0)
-		printf("ERROR opening message queue\n");
+	server = mq_open (SERVER_QUEUE_NAME, O_RDWR | O_CREAT, QUEUE_PERMISSIONS, &attr);
+	if (server < 0) 
+        printf("ERROR opening message queue\n");
 	
-	//char* buffrec = (char*)(&mesg_struct);
-	//receive a message from a message queue
-	char *payload_cptr;
-    	message payloadRecv = {0};
-    	payload_cptr = (char*)&payloadRecv;
-	
-	if (mq_receive (server, payload_cptr, sizeof(mesg_struct), 0) < 0)
+	if (mq_receive (server, buff, sizeof(message), NULL) < 0)
 		printf("ERROR mq_receive\n");
-	else printf ("Process 1: message received.\n");
+	else printf ("Process 2: message received.\n");
+	//send message
+	char* buffptr = (char*)&mesg_struct;
+	strcpy(mesg_struct.string, "From Process 2 to Process 1 msg\n");
+    mesg_struct.string_length = strlen(mesg_struct.string);
+    mesg_struct.USR_Led_Control = 1;
+	
+	if (mq_send (server, buffptr, sizeof(mesg_struct), 0) == -1)
+		printf("ERROR mq_send\n");
+	else printf("Process 2: message sent\n");
+	
+	
+	//receive message
+	//char* buffrec = (char*)(&buff);
 	
 	ptr = (message*)(buff);
-    printf("Process1 Received string: %s, String length = %d, USR led status: %d\n", ptr->string, ptr->string_length, ptr->USR_Led_Control);
-	
-	//send message to process 2
-	strcpy(mesg_struct.string, "From Process 1 to Process 2 msg\n");
-    mesg_struct.string_length = strlen(mesg_struct.string);
-    mesg_struct.USR_Led_Control = 0;
-	char* buffptr = (char*)(&mesg_struct);
-	if (mq_send (server, buffptr, sizeof(message), 0) < 0)
-		printf("ERROR mq_send\n");
-	else printf("Process 1: message sent\n");
+    printf("Process2 Received string: %s, String length = %d, USR led status: %d\n", ptr->string, ptr->string_length, ptr->USR_Led_Control);
 
-	mq_close(server);
+	mq_unlink(SERVER_QUEUE_NAME);
 	
-	
-	return 0;
 }
+
+
+  
