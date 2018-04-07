c698ccc4afad8d0b4e658f4dc24bf316fa064b07
Vipraja Patil <vipa4143@colorado.edu>
2018-04-07 14:59:35 -0600

    Updated Problem 3

--- HW5/Problem3/main.c
+++ HW5/Problem3/main.c
@@ -48,16 +48,48 @@ __error__(char *pcFilename, uint32_t ui32Line)
 }
 #endif
 
-void task1 (TimerHandle_t xTimer1)
+void timerTask1 (TimerHandle_t xTimer1)
 {
         GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, a);
         a ^= GPIO_PIN_0;
 }
 
-void task2 (TimerHandle_t xTimer2)
+void timerTask2 (TimerHandle_t xTimer2)
 {
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, b);
         b ^= GPIO_PIN_4;
+}
+
+void task1(void *pvParameters)
+{
+    TimerHandle_t xTimer1 = NULL;
+       xTimer1 = xTimerCreate(
+               "Timer1", /* A text name to help debugging */
+               pdMS_TO_TICKS(500), /* Timer period, i.e. 1 s */
+               pdTRUE,  /* Timer type, auto-reload or one-shot*/
+               (void *) pvTimerGetTimerID(xTimer1), /* Identifier of created timer */
+               timerTask1   /* Callback function */
+             );
+       xTimerStart(xTimer1, 0);
+
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+    while(1){}
+}
+
+void task2(void *pvParameters)
+{
+    TimerHandle_t xTimer2 = NULL;
+        xTimer2 = xTimerCreate(
+                      "Timer2", /* A text name to help debugging */
+                      pdMS_TO_TICKS(250), /* Timer period, i.e. 1 s */
+                      pdTRUE,  /* Timer type, auto-reload or one-shot*/
+                      (void *) pvTimerGetTimerID(xTimer2), /* Identifier of created timer */
+                      timerTask2   /* Callback function */
+                    );
+        xTimerStart(xTimer2, 0);
+
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4);
+    while(1){}
 }
 
 // Main function
@@ -69,31 +101,11 @@ int main(void)
     // Configure the device pins.
     PinoutSet(false, false);
 
-    TimerHandle_t xTimer1 = NULL;
-    xTimer1 = xTimerCreate(
-            "Timer1", /* A text name to help debugging */
-            pdMS_TO_TICKS(500), /* Timer period, i.e. 1 s */
-            pdTRUE,  /* Timer type, auto-reload or one-shot*/
-            (void *) pvTimerGetTimerID(xTimer1), /* Identifier of created timer */
-            task1   /* Callback function */
-          );
+    xTaskCreate(task1, (const portCHAR *)"Task1", 1024, NULL, 1, NULL);
+    xTaskCreate(task2, (const portCHAR *)"Task2", 1024, NULL, 1, NULL);
 
-    TimerHandle_t xTimer2 = NULL;
-    xTimer2 = xTimerCreate(
-                  "Timer2", /* A text name to help debugging */
-                  pdMS_TO_TICKS(250), /* Timer period, i.e. 1 s */
-                  pdTRUE,  /* Timer type, auto-reload or one-shot*/
-                  (void *) pvTimerGetTimerID(xTimer2), /* Identifier of created timer */
-                  task2   /* Callback function */
-                );
+    vTaskStartScheduler();
 
-    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4);
-
-    if(xTimerStart(xTimer1, 500) && xTimerStart(xTimer2, 500))
-    {
-        vTaskStartScheduler();
-    }
 
     return 0;
 }
